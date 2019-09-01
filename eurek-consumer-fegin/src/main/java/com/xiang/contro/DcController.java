bb20b7a0fbf857203ecd11bb6b16f0ca5234e3c1
unknown <18233184432@163.com>
2019-09-01 14:18:15 +0800

    eurake

---
+++ eurek-consumer-fegin/src/main/java/com/xiang/contro/DcController.java
@@ -0,0 +1,19 @@
+package com.xiang.contro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xiang.inter.DcClient;
+@RestController
+public class DcController {
+
+    @Autowired
+    DcClient dcClient;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return dcClient.consumer();
+    }
+
+}
